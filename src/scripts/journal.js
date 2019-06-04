ae50e28d746bfed8b4890a7aa4813a1fb30257c3
Tyler Carpenter <tcarp55@gmail.com>
2019-06-04 18:06:16 -0500

    added objects to store journal entries in js file

---
+++ src/scripts/journal.js
@@ -0,0 +1,31 @@
+/*
+
+
+    Date of the journal entry.
+    Concepts covered (which will be the title of the entry).
+    The long-form contents of the journal entry.
+    The mood of the journal entry.
+
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const entryArray = []
+
+const journalEntryMonday = {
+    date: `6/3/19`,
+    Concept: `dot method over bracket method`,
+    entry: `i feel that dot method is quicker and easier but bracket method is more useful.`,
+    mood: `confused`
+}
+
+entryArray.push(journalEntryMonday)
+
+const journalEntryTuesday = {
+    date: `6/4/19`,
+    Concept: `using objects`,
+    entry: `learned that an object can also hold a function. its called a method.`,
+    mood: `excited`
+}
+
+entryArray.push(journalEntryTuesday)
+
