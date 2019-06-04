a62bf68344987a7900a165801b8e90c7ca019724
Tyler Carpenter <tcarp55@gmail.com>
2019-06-04 18:45:54 -0500

    added fucntion to put journal entry objects into the DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -12,20 +12,42 @@
 const entryArray = []
 
 const journalEntryMonday = {
-    date: `6/3/19`,
-    Concept: `dot method over bracket method`,
-    entry: `i feel that dot method is quicker and easier but bracket method is more useful.`,
-    mood: `confused`
+    Date: `6/3/19`,
+    Concept: `Dot method vs bracket method`,
+    Entry: `i feel that dot method is quicker and easier but bracket method is more useful.`,
+    Mood: `confused`
 }
 
 entryArray.push(journalEntryMonday)
 
 const journalEntryTuesday = {
-    date: `6/4/19`,
-    Concept: `using objects`,
-    entry: `learned that an object can also hold a function. its called a method.`,
-    mood: `excited`
+    Date: `6/4/19`,
+    Concept: `Using Objects`,
+    Entry: `learned that an object can also hold a function. its called a method.`,
+    Mood: `excited`
 }
 
 entryArray.push(journalEntryTuesday)
+
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+
+    Arguments: journalEntry (object)
+*/
+const thisPoints = document.querySelector(".entries")
+
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return`
+     <h2> ${journalEntry.Concept} </h2>
+    <section>
+        <article>
+            <p> ${journalEntry.Entry} <br> ${journalEntry.Mood} <br> ${journalEntry.Date} </p>
+        </article>
+    </section>`
+}
+entryArray.forEach( function(entries){
+    thisPoints.innerHTML += makeJournalEntryComponent(entries)});
+// thisPoints.innerHTML += makeJournalEntryComponent()
 
