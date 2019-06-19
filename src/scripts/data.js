b419d87f5cf574001add9c874c7bbb157bb52978
Tyler Carpenter <tcarp55@gmail.com>
2019-06-18 19:42:06 -0500

    added a click event that will add the journal entry to json file

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,4 +1,3 @@
-
 // API.getJournalEntries()
 // .then(entries => {
 //     console.log(entries)
@@ -9,10 +8,21 @@
 //     })
 // })
 
-
 const API = {
-    getJournalEntries () {
-        return fetch("http://localhost:3000/entries")
-        .then(response => response.json())
-    }
-}
+  getJournalEntries() {
+    return fetch("http://localhost:3000/entries").then(response =>
+      response.json()
+    );
+  },
+  saveJournalEntry(newEntry) {
+    // Use `fetch` with the POST method to add your entry to your API
+    fetch("http://localhost:3000/entries", {
+      // Replace "url" with your API's URL
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newEntry)
+    });
+  }
+};
--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,10 +1,10 @@
-const makeJournalEntryComponent = (journalEntry) => {
-    // Create your own HTML structure for a journal entry
-    return`
+const makeJournalEntryComponent = journalEntry => {
+  // Create your own HTML structure for a journal entry
+  return `
      <h2> ${journalEntry.Concept} </h2>
     <section>
         <article>
             <p> ${journalEntry.Entry} <br> ${journalEntry.Mood} <br> ${journalEntry.Date} </p>
         </article>
-    </section>`
-}
+    </section>`;
+};
